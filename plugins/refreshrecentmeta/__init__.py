029a2a2ea8c6462447e6e199c5328b6851ded74e
dandkong <1483168696@qq.com>
2023-11-07 21:33:35 +0800

    测试

--- plugins/refreshrecentmeta/__init__.py
+++ plugins/refreshrecentmeta/__init__.py
@@ -35,7 +35,7 @@ class RefreshRecentMeta(_PluginBase):
     # 插件配置项ID前缀
     plugin_config_prefix = "refreshrecentmeta_"
     # 加载顺序
-    plugin_order = 17
+    plugin_order = 99
     # 可使用的用户级别
     auth_level = 1
 
