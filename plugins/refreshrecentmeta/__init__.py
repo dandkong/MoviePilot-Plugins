5362b1a90aae8913a629e8c505f438c14272f17a
dandkong <1483168696@qq.com>
2023-11-07 23:20:32 +0800

    1.0

--- plugins/refreshrecentmeta/__init__.py
+++ plugins/refreshrecentmeta/__init__.py
@@ -15,13 +15,15 @@ from app.plugins import _PluginBase
 from typing import Any, List, Dict, Tuple, Optional
 from app.log import logger
 from app.schemas import NotificationType
+import datetime
+from app.utils.http import RequestUtils
 
 
 class RefreshRecentMeta(_PluginBase):
     # 插件名称
-    plugin_name = "自动刷新最近媒体元数据"
+    plugin_name = "自动刷新最近加入媒体元数据"
     # 插件描述
-    plugin_desc = "自动刷新最近媒体元数据"
+    plugin_desc = "自动刷新最近加入媒体元数据"
     # 插件图标
     plugin_icon = "backup.png"
     # 主题色
@@ -43,7 +45,7 @@ class RefreshRecentMeta(_PluginBase):
     _enabled = False
     # 任务执行间隔
     _cron = None
-    _cnt = None
+    _offset_days = "0"
     _onlyonce = False
     _notify = False
 
@@ -57,7 +59,7 @@ class RefreshRecentMeta(_PluginBase):
         if config:
             self._enabled = config.get("enabled")
             self._cron = config.get("cron")
-            self._cnt = config.get("cnt")
+            self._offset_days = config.get("offset_days")
             self._notify = config.get("notify")
             self._onlyonce = config.get("onlyonce")
 
@@ -68,24 +70,24 @@ class RefreshRecentMeta(_PluginBase):
 
             if self._cron:
                 try:
-                    self._scheduler.add_job(func=self.__backup,
+                    self._scheduler.add_job(func=self.__refresh_recent,
                                             trigger=CronTrigger.from_crontab(self._cron),
-                                            name="自动备份")
+                                            name="自动刷新最近加入媒体元数据")
                 except Exception as err:
                     logger.error(f"定时任务配置错误：{str(err)}")
 
             if self._onlyonce:
-                logger.info(f"自动备份服务启动，立即运行一次")
-                self._scheduler.add_job(func=self.__backup, trigger='date',
+                logger.info(f"自动刷新最近媒体元数据服务启动，立即运行一次")
+                self._scheduler.add_job(func=self.__refresh_recent, trigger='date',
                                         run_date=datetime.now(tz=pytz.timezone(settings.TZ)) + timedelta(seconds=3),
-                                        name="自动备份")
+                                        name="自动刷新最近加入媒体元数据")
                 # 关闭一次性开关
                 self._onlyonce = False
                 self.update_config({
                     "onlyonce": False,
                     "cron": self._cron,
                     "enabled": self._enabled,
-                    "cnt": self._cnt,
+                    "offset_days": self._offset_days,
                     "notify": self._notify,
                 })
 
@@ -94,83 +96,49 @@ class RefreshRecentMeta(_PluginBase):
                 self._scheduler.print_jobs()
                 self._scheduler.start()
 
-    def __backup(self):
-        """
-        自动备份、删除备份
-        """
-        logger.info(f"当前时间 {time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))} 开始备份")
+    def __get_date(self, offset_day):
+        now_time = datetime.datetime.now()
+        end_time = now_time + datetime.timedelta(days=offset_day)
+        end_date = end_time.strftime("%Y-%m-%d")
+        return end_date
 
-        # docker用默认路径
-        bk_path = self.get_data_path()
+    def __refresh_recent(self):
+        if "emby" not in settings.MEDIASERVER:
+            return
 
-        # 备份
-        zip_file = self.backup_file(bk_path=bk_path)
+        logger.info(
+            f"当前时间 {time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))} 自动刷新最近加入媒体元数据")
 
-        if zip_file:
-            success = True
-            msg = f"备份完成 备份文件 {zip_file}"
-            logger.info(msg)
-        else:
-            success = False
-            msg = "创建备份失败"
-            logger.error(msg)
-
-        # 清理备份
-        bk_cnt = 0
-        del_cnt = 0
-        if self._cnt:
-            # 获取指定路径下所有以"bk"开头的文件，按照创建时间从旧到新排序
-            files = sorted(glob.glob(f"{bk_path}/bk**"), key=os.path.getctime)
-            bk_cnt = len(files)
-            # 计算需要删除的文件数
-            del_cnt = bk_cnt - int(self._cnt)
-            if del_cnt > 0:
-                logger.info(
-                    f"获取到 {bk_path} 路径下备份文件数量 {bk_cnt} 保留数量 {int(self._cnt)} 需要删除备份文件数量 {del_cnt}")
-
-                # 遍历并删除最旧的几个备份
-                for i in range(del_cnt):
-                    os.remove(files[i])
-                    logger.debug(f"删除备份文件 {files[i]} 成功")
-            else:
-                logger.info(
-                    f"获取到 {bk_path} 路径下备份文件数量 {bk_cnt} 保留数量 {int(self._cnt)} 无需删除")
-
-        # 发送通知
-        if self._notify:
-            self.post_message(
-                mtype=NotificationType.SiteMessage,
-                title="【自动备份任务完成】",
-                text=f"创建备份{'成功' if zip_file else '失败'}\n"
-                     f"清理备份数量 {del_cnt}\n"
-                     f"剩余备份数量 {bk_cnt - del_cnt}")
-
-        return success, msg
-
-    @staticmethod
-    def backup_file(bk_path: Path = None):
-        """
-        @param bk_path     自定义备份路径
-        """
-        try:
-            # 创建备份文件夹
-            config_path = Path(settings.CONFIG_PATH)
-            backup_file = f"bk_{time.strftime('%Y%m%d%H%M%S')}"
-            backup_path = bk_path / backup_file
-            backup_path.mkdir(parents=True)
-            # 把现有的相关文件进行copy备份
-            if settings.LIBRARY_CATEGORY:
-                shutil.copy(f'{config_path}/category.yaml', backup_path)
-            shutil.copy(f'{config_path}/user.db', backup_path)
-
-            zip_file = str(backup_path) + '.zip'
-            if os.path.exists(zip_file):
-                zip_file = str(backup_path) + '.zip'
-            shutil.make_archive(str(backup_path), 'zip', str(backup_path))
-            shutil.rmtree(str(backup_path))
-            return zip_file
-        except IOError:
+        host = settings.EMBY_HOST
+        apikey = settings.EMBY_API_KEY
+        if not host or not apikey:
             return None
+        end_date = self.__get_date(-int(self._offset_days))
+        # 获得_offset_day加入的剧集
+        req_url = "%semby/Items?IncludeItemTypes=Episode&MinPremiereDate=%s&IsMissing=false&Recursive=true&api_key=%s" % (
+            host, end_date, apikey)
+        try:
+            res = RequestUtils().get_res(req_url)
+            if res:
+                res_items = res.json().get("Items")
+                if res_items:
+                    for res_item in res_items:
+                        item_id = res_item.get('Id')
+                        series_name = res_item.get('SeriesName')
+                        name = res_item.get('Name')
+                        # 刷新元数据
+                        req_url = "%semby/Items/%s/Refresh?MetadataRefreshMode=FullRefresh&ImageRefreshMode=FullRefresh&ReplaceAllMetadata=true&ReplaceAllImages=true&api_key=%s" % (
+                            host, item_id, apikey)
+                        try:
+                            res = RequestUtils().post_res(req_url)
+                            if res:
+                                logger.info(f"刷新元数据：{series_name} - {name}")
+                            else:
+                                logger.error(f"刷新媒体库对象 {item_id} 失败，无法连接Emby！")
+                        except Exception as e:
+                            logger.error(f"连接Items/Id/Refresh出错：" + str(e))
+        except Exception as e:
+            logger.error(f"连接Items出错：" + str(e))
 
     def get_state(self) -> bool:
         return self._enabled
@@ -180,23 +148,7 @@ class RefreshRecentMeta(_PluginBase):
         pass
 
     def get_api(self) -> List[Dict[str, Any]]:
-        return [{
-            "path": "/backup",
-            "endpoint": self.__backup,
-            "methods": ["GET"],
-            "summary": "MoviePilot备份",
-            "description": "MoviePilot备份",
-        }]
-
-    def backup(self) -> schemas.Response:
-        """
-        API调用备份
-        """
-        success, msg = self.__backup()
-        return schemas.Response(
-            success=success,
-            message=msg
-        )
+        pass
 
     def get_form(self) -> Tuple[List[dict], Dict[str, Any]]:
         """
@@ -273,7 +225,7 @@ class RefreshRecentMeta(_PluginBase):
                                         'component': 'VTextField',
                                         'props': {
                                             'model': 'cron',
-                                            'label': '备份周期'
+                                            'label': '执行周期'
                                         }
                                     }
                                 ]
@@ -288,8 +240,8 @@ class RefreshRecentMeta(_PluginBase):
                                     {
                                         'component': 'VTextField',
                                         'props': {
-                                            'model': 'cnt',
-                                            'label': '最大保留备份数'
+                                            'model': 'offset_days',
+                                            'label': '几天内'
                                         }
                                     }
                                 ]
