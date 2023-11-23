9ade83d05d1f1916342cd1c6b558469bd3f68dd2
dandkong <1483168696@qq.com>
2023-11-23 17:34:38 +0800

    测试
    测试

--- plugins/refreshrecentmeta/__init__.py
+++ plugins/refreshrecentmeta/__init__.py
@@ -98,6 +98,7 @@ class RefreshRecentMeta(_PluginBase):
 
     @eventmanager.register(EventType.PluginAction)
     def __refresh_recent(self, event: Event = None):
+        logger.info("收到事件~~~~~~~~~~~~~~~~~~~~~~~~~")
         if event:
             event_data = event.event_data
             if not event_data or event_data.get("action") != "refreshrecentmeta":
