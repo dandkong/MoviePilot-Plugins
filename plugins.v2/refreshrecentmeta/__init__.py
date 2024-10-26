170b394fed732c89810bf1ed1740b7c49c0c4720
dandkong <1483168696@qq.com>
2024-10-26 22:52:54 +0800

    测试

--- plugins.v2/refreshrecentmeta/__init__.py
+++ plugins.v2/refreshrecentmeta/__init__.py
@@ -21,7 +21,7 @@ class RefreshRecentMeta(_PluginBase):
     # 插件图标
     plugin_icon = "backup.png"
     # 插件版本
-    plugin_version = "1.0"
+    plugin_version = "1.1"
     # 插件作者
     plugin_author = "dandkong"
     # 作者主页
